f7ccf54ea039c80ffb527d2dacc70c8401eda260
Riccardo Fani <riccardofani@gmail.com>
2025-03-16 22:56:46 +0100

    all kernel functions && all conv functions signature added

---
+++ libftprintf.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rfani <rfani@student.42firenze.it>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/14 18:10:26 by rfani             #+#    #+#             */
+/*   Updated: 2025/03/16 22:48:22 by rfani            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+# include "libft/libft.h"
+# include <__stdarg_va_list.h>
+# include <stdarg.h>
+
+typedef struct s_arg
+{
+	char	*content;
+	int		length;
+}	t_arg;
+int		ft_printf(const char *format, ...);
+int		ft_vprintf(const char *format, va_list args);
+t_arg	ft_parse_convspec(const char **format, va_list args);
+int		ft_print_arg(t_arg arg);
+t_arg	ft_conv_char(va_list args);
+t_arg	ft_conv_str(va_list args);
+t_arg	ft_conv_ptr(va_list args);
+t_arg	ft_conv_int(va_list args);
+t_arg	ft_conv_uint(va_list args);
+t_arg	ft_conv_hex(va_list args);
+t_arg	ft_conv_HEX(va_list args);
+t_arg	ft_conv_percent(va_list args);
+
+#endif
