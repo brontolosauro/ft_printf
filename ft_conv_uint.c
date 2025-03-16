dcfb15d508402ca3d48dbea7201414f43785f343
Riccardo Fani <riccardofani@gmail.com>
2025-03-17 00:24:17 +0100

    ft_uint done; needs ft_uitoa done

--- ft_conv_uint.c
+++ ft_conv_uint.c
@@ -6,7 +6,25 @@
 /*   By: rfani <rfani@student.42firenze.it>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 22:52:46 by rfani             #+#    #+#             */
-/*   Updated: 2025/03/16 22:52:47 by rfani            ###   ########.fr       */
+/*   Updated: 2025/03/17 00:03:49 by rfani            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libftprintf.h"
+#include "libft/libft.h"
+#include <stdarg.h>
+
+t_arg	ft_conv_char(va_list args);
+
+t_arg	ft_conv_char(va_list args)
+{
+	t_arg			arg;
+	unsigned int	n;
+	char			*str;
+
+	n = va_arg(args, unsigned int);
+	str = ft_uitoa(n);
+	arg.content = str;
+	arg.length = ft_strlen(str);
+	return (arg);
+}
--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: rfani <rfani@student.42firenze.it>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 18:10:26 by rfani             #+#    #+#             */
-/*   Updated: 2025/03/16 23:16:08 by rfani            ###   ########.fr       */
+/*   Updated: 2025/03/17 00:05:13 by rfani            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,5 +32,6 @@ t_arg	ft_conv_uint(va_list args);
 t_arg	ft_conv_hex(va_list args);
 t_arg	ft_conv_HEX(va_list args);
 t_arg	ft_conv_percent(void);
+char	*ft_uitoa(unsigned int n);
 
 #endif
