23b83c4efe5785a3eb45d3d94a6b4dcd95fe18b7
Riccardo Fani <riccardofani@gmail.com>
2025-03-17 00:02:08 +0100

    ft_conv_int done

--- ft_conv_int.c
+++ ft_conv_int.c
@@ -6,7 +6,25 @@
 /*   By: rfani <rfani@student.42firenze.it>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 22:52:28 by rfani             #+#    #+#             */
-/*   Updated: 2025/03/16 22:52:29 by rfani            ###   ########.fr       */
+/*   Updated: 2025/03/16 23:50:22 by rfani            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libftprintf.h"
+#include "libft/libft.h"
+#include <stdarg.h>
+
+t_arg	ft_conv_char(va_list args);
+
+t_arg	ft_conv_char(va_list args)
+{
+	t_arg	arg;
+	int		n;
+	char	*str;
+
+	n = va_arg(args, int);
+	str = ft_itoa(n);
+	arg.content = str;
+	arg.length = ft_strlen(str);
+	return (arg);
+}
