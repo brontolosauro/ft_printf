182d388d31aae28d5b95be7afbf3feb3897724be
Riccardo Fani <riccardofani@gmail.com>
2025-03-17 23:33:09 +0100

    add VSCode configuration files and main function for testing; fixed ft_conv_chex; all test passed

--- main.c
+++ main.c
@@ -1,39 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_conv_chex.c                                     :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rfani <rfani@student.42firenze.it>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/16 22:52:25 by rfani             #+#    #+#             */
-/*   Updated: 2025/03/17 23:03:38 by rfani            ###   ########.fr       */
+/*   Created: 2025/03/17 19:55:30 by rfani             #+#    #+#             */
+/*   Updated: 2025/03/17 23:30:57 by rfani            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdarg.h>
+#include <stdio.h>
+#include <limits.h>
 #include "libft/libft.h"
 #include "ft_printf.h"
 
-t_arg	ft_conv_chex(va_list args);
-
-t_arg	ft_conv_chex(va_list args)
+int	main(void)
 {
-	t_arg			arg;
-	unsigned int	n;
-	char			*str;
-	int				str_len;
-	int				i;
-
-	n = va_arg(args, unsigned int);
-	str = ft_xitoa(n);
-	str_len = ft_strlen(str);
-	i = 0;
-	while (i < str_len)
-	{
-		str[i] = (char)ft_toupper((int)str[i]);
-		i++;
-	}
-	arg.content = str;
-	arg.length = ft_strlen(str);
-	return (arg);
+	printf(" %c %c %c \n", '0', 0, '1');
+	ft_printf(" %c %c %c \n", '0', 0, '1');
+	return (0);
 }
